7f030e6354afe56bfea76117087b43abebe4a6de
Nico Armanini <armanininico@gmail.com>
2022-09-27 23:39:06 -0300

    proyecto completo

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -65,26 +65,28 @@
             this.mrcTipo.Controls.Add(this.lblDias);
             this.mrcTipo.Controls.Add(this.lblPersonas);
             this.mrcTipo.Controls.Add(this.lblTipos);
-            this.mrcTipo.Location = new System.Drawing.Point(30, 24);
+            this.mrcTipo.Location = new System.Drawing.Point(40, 30);
+            this.mrcTipo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mrcTipo.Name = "mrcTipo";
-            this.mrcTipo.Size = new System.Drawing.Size(548, 77);
+            this.mrcTipo.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mrcTipo.Size = new System.Drawing.Size(731, 95);
             this.mrcTipo.TabIndex = 0;
             this.mrcTipo.TabStop = false;
             this.mrcTipo.Text = "Tipo de Cabania";
             // 
             // txtDias
             // 
-            this.txtDias.Enabled = false;
-            this.txtDias.Location = new System.Drawing.Point(390, 32);
+            this.txtDias.Location = new System.Drawing.Point(520, 39);
+            this.txtDias.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtDias.Name = "txtDias";
-            this.txtDias.Size = new System.Drawing.Size(90, 20);
+            this.txtDias.Size = new System.Drawing.Size(119, 22);
             this.txtDias.TabIndex = 4;
             this.txtDias.TextChanged += new System.EventHandler(this.txtDias_TextChanged);
+            this.txtDias.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDias_KeyPress);
             // 
             // cmbPersonas
             // 
             this.cmbPersonas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbPersonas.Enabled = false;
             this.cmbPersonas.FormattingEnabled = true;
             this.cmbPersonas.Items.AddRange(new object[] {
             "1",
@@ -93,9 +95,10 @@
             "4",
             "5",
             "6"});
-            this.cmbPersonas.Location = new System.Drawing.Point(232, 32);
+            this.cmbPersonas.Location = new System.Drawing.Point(309, 39);
+            this.cmbPersonas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbPersonas.Name = "cmbPersonas";
-            this.cmbPersonas.Size = new System.Drawing.Size(76, 21);
+            this.cmbPersonas.Size = new System.Drawing.Size(100, 24);
             this.cmbPersonas.TabIndex = 3;
             this.cmbPersonas.SelectedIndexChanged += new System.EventHandler(this.cmbPersonas_SelectedIndexChanged);
             // 
@@ -106,36 +109,40 @@
             this.cmbTipos.Items.AddRange(new object[] {
             "A",
             "B"});
-            this.cmbTipos.Location = new System.Drawing.Point(66, 29);
+            this.cmbTipos.Location = new System.Drawing.Point(88, 36);
+            this.cmbTipos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbTipos.Name = "cmbTipos";
-            this.cmbTipos.Size = new System.Drawing.Size(76, 21);
+            this.cmbTipos.Size = new System.Drawing.Size(100, 24);
             this.cmbTipos.TabIndex = 3;
             this.cmbTipos.SelectedIndexChanged += new System.EventHandler(this.cmbTipos_SelectedIndexChanged);
             // 
             // lblDias
             // 
             this.lblDias.AutoSize = true;
-            this.lblDias.Location = new System.Drawing.Point(356, 35);
+            this.lblDias.Location = new System.Drawing.Point(475, 43);
+            this.lblDias.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDias.Name = "lblDias";
-            this.lblDias.Size = new System.Drawing.Size(28, 13);
+            this.lblDias.Size = new System.Drawing.Size(35, 16);
             this.lblDias.TabIndex = 2;
             this.lblDias.Text = "Dias";
             // 
             // lblPersonas
             // 
             this.lblPersonas.AutoSize = true;
-            this.lblPersonas.Location = new System.Drawing.Point(175, 35);
+            this.lblPersonas.Location = new System.Drawing.Point(233, 43);
+            this.lblPersonas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPersonas.Name = "lblPersonas";
-            this.lblPersonas.Size = new System.Drawing.Size(51, 13);
+            this.lblPersonas.Size = new System.Drawing.Size(65, 16);
             this.lblPersonas.TabIndex = 1;
             this.lblPersonas.Text = "Personas";
             // 
             // lblTipos
             // 
             this.lblTipos.AutoSize = true;
-            this.lblTipos.Location = new System.Drawing.Point(27, 32);
+            this.lblTipos.Location = new System.Drawing.Point(36, 39);
+            this.lblTipos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTipos.Name = "lblTipos";
-            this.lblTipos.Size = new System.Drawing.Size(33, 13);
+            this.lblTipos.Size = new System.Drawing.Size(42, 16);
             this.lblTipos.TabIndex = 0;
             this.lblTipos.Text = "Tipos";
             this.lblTipos.Click += new System.EventHandler(this.label1_Click);
@@ -146,9 +153,11 @@
             this.mrcAdicionales.Controls.Add(this.chkHeladera);
             this.mrcAdicionales.Controls.Add(this.chkCocina);
             this.mrcAdicionales.Enabled = false;
-            this.mrcAdicionales.Location = new System.Drawing.Point(27, 107);
+            this.mrcAdicionales.Location = new System.Drawing.Point(36, 132);
+            this.mrcAdicionales.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mrcAdicionales.Name = "mrcAdicionales";
-            this.mrcAdicionales.Size = new System.Drawing.Size(160, 118);
+            this.mrcAdicionales.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mrcAdicionales.Size = new System.Drawing.Size(213, 145);
             this.mrcAdicionales.TabIndex = 1;
             this.mrcAdicionales.TabStop = false;
             this.mrcAdicionales.Text = "Adicionales";
@@ -156,9 +165,10 @@
             // chkTele
             // 
             this.chkTele.AutoSize = true;
-            this.chkTele.Location = new System.Drawing.Point(33, 74);
+            this.chkTele.Location = new System.Drawing.Point(44, 91);
+            this.chkTele.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkTele.Name = "chkTele";
-            this.chkTele.Size = new System.Drawing.Size(74, 17);
+            this.chkTele.Size = new System.Drawing.Size(92, 20);
             this.chkTele.TabIndex = 0;
             this.chkTele.Text = "Television";
             this.chkTele.UseVisualStyleBackColor = true;
@@ -166,9 +176,10 @@
             // chkHeladera
             // 
             this.chkHeladera.AutoSize = true;
-            this.chkHeladera.Location = new System.Drawing.Point(33, 51);
+            this.chkHeladera.Location = new System.Drawing.Point(44, 63);
+            this.chkHeladera.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkHeladera.Name = "chkHeladera";
-            this.chkHeladera.Size = new System.Drawing.Size(69, 17);
+            this.chkHeladera.Size = new System.Drawing.Size(86, 20);
             this.chkHeladera.TabIndex = 0;
             this.chkHeladera.Text = "Heladera";
             this.chkHeladera.UseVisualStyleBackColor = true;
@@ -176,9 +187,10 @@
             // chkCocina
             // 
             this.chkCocina.AutoSize = true;
-            this.chkCocina.Location = new System.Drawing.Point(33, 28);
+            this.chkCocina.Location = new System.Drawing.Point(44, 34);
+            this.chkCocina.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chkCocina.Name = "chkCocina";
-            this.chkCocina.Size = new System.Drawing.Size(59, 17);
+            this.chkCocina.Size = new System.Drawing.Size(71, 20);
             this.chkCocina.TabIndex = 0;
             this.chkCocina.Text = "Cocina";
             this.chkCocina.UseVisualStyleBackColor = true;
@@ -190,9 +202,11 @@
             this.mrcPagos.Controls.Add(this.cmbTarjetas);
             this.mrcPagos.Controls.Add(this.lblTarjetas);
             this.mrcPagos.Enabled = false;
-            this.mrcPagos.Location = new System.Drawing.Point(208, 107);
+            this.mrcPagos.Location = new System.Drawing.Point(277, 132);
+            this.mrcPagos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mrcPagos.Name = "mrcPagos";
-            this.mrcPagos.Size = new System.Drawing.Size(370, 118);
+            this.mrcPagos.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mrcPagos.Size = new System.Drawing.Size(493, 145);
             this.mrcPagos.TabIndex = 1;
             this.mrcPagos.TabStop = false;
             this.mrcPagos.Text = "Forma de Pago";
@@ -200,9 +214,10 @@
             // optTarjeta
             // 
             this.optTarjeta.AutoSize = true;
-            this.optTarjeta.Location = new System.Drawing.Point(165, 35);
+            this.optTarjeta.Location = new System.Drawing.Point(220, 43);
+            this.optTarjeta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.optTarjeta.Name = "optTarjeta";
-            this.optTarjeta.Size = new System.Drawing.Size(58, 17);
+            this.optTarjeta.Size = new System.Drawing.Size(71, 20);
             this.optTarjeta.TabIndex = 0;
             this.optTarjeta.TabStop = true;
             this.optTarjeta.Text = "Tarjeta";
@@ -212,9 +227,10 @@
             // optEfectivo
             // 
             this.optEfectivo.AutoSize = true;
-            this.optEfectivo.Location = new System.Drawing.Point(35, 35);
+            this.optEfectivo.Location = new System.Drawing.Point(47, 43);
+            this.optEfectivo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.optEfectivo.Name = "optEfectivo";
-            this.optEfectivo.Size = new System.Drawing.Size(64, 17);
+            this.optEfectivo.Size = new System.Drawing.Size(76, 20);
             this.optEfectivo.TabIndex = 0;
             this.optEfectivo.TabStop = true;
             this.optEfectivo.Text = "Efectivo";
@@ -224,24 +240,26 @@
             // cmbTarjetas
             // 
             this.cmbTarjetas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTarjetas.Enabled = false;
             this.cmbTarjetas.FormattingEnabled = true;
             this.cmbTarjetas.Items.AddRange(new object[] {
-            "Master\t",
-            "Visa",
-            "Macro",
-            "Santander"});
-            this.cmbTarjetas.Location = new System.Drawing.Point(35, 90);
+            "Card Red",
+            "Card Green",
+            "Card Blue"});
+            this.cmbTarjetas.Location = new System.Drawing.Point(47, 111);
+            this.cmbTarjetas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbTarjetas.Name = "cmbTarjetas";
-            this.cmbTarjetas.Size = new System.Drawing.Size(267, 21);
+            this.cmbTarjetas.Size = new System.Drawing.Size(355, 24);
             this.cmbTarjetas.TabIndex = 3;
             this.cmbTarjetas.SelectedIndexChanged += new System.EventHandler(this.cmbTarjetas_SelectedIndexChanged);
             // 
             // lblTarjetas
             // 
             this.lblTarjetas.AutoSize = true;
-            this.lblTarjetas.Location = new System.Drawing.Point(32, 74);
+            this.lblTarjetas.Location = new System.Drawing.Point(43, 91);
+            this.lblTarjetas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTarjetas.Name = "lblTarjetas";
-            this.lblTarjetas.Size = new System.Drawing.Size(45, 13);
+            this.lblTarjetas.Size = new System.Drawing.Size(57, 16);
             this.lblTarjetas.TabIndex = 0;
             this.lblTarjetas.Text = "Tarjetas";
             this.lblTarjetas.Click += new System.EventHandler(this.label1_Click);
@@ -253,68 +271,83 @@
             this.mrcTitular.Controls.Add(this.lblTelefono);
             this.mrcTitular.Controls.Add(this.lblNombre);
             this.mrcTitular.Enabled = false;
-            this.mrcTitular.Location = new System.Drawing.Point(27, 231);
+            this.mrcTitular.Location = new System.Drawing.Point(36, 284);
+            this.mrcTitular.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mrcTitular.Name = "mrcTitular";
-            this.mrcTitular.Size = new System.Drawing.Size(551, 118);
+            this.mrcTitular.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mrcTitular.Size = new System.Drawing.Size(735, 145);
             this.mrcTitular.TabIndex = 1;
             this.mrcTitular.TabStop = false;
             this.mrcTitular.Text = "Titular de la Reserva";
             // 
             // txtTelefono
             // 
-            this.txtTelefono.Location = new System.Drawing.Point(107, 66);
+            this.txtTelefono.Enabled = false;
+            this.txtTelefono.Location = new System.Drawing.Point(143, 81);
+            this.txtTelefono.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtTelefono.Name = "txtTelefono";
-            this.txtTelefono.Size = new System.Drawing.Size(407, 20);
+            this.txtTelefono.Size = new System.Drawing.Size(541, 22);
             this.txtTelefono.TabIndex = 4;
+            this.txtTelefono.TextChanged += new System.EventHandler(this.txtTelefono_TextChanged);
+            this.txtTelefono.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTelefono_KeyPress);
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(107, 35);
+            this.txtNombre.Location = new System.Drawing.Point(143, 43);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(407, 20);
+            this.txtNombre.Size = new System.Drawing.Size(541, 22);
             this.txtNombre.TabIndex = 4;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
             // 
             // lblTelefono
             // 
             this.lblTelefono.AutoSize = true;
-            this.lblTelefono.Location = new System.Drawing.Point(30, 69);
+            this.lblTelefono.Location = new System.Drawing.Point(40, 85);
+            this.lblTelefono.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTelefono.Name = "lblTelefono";
-            this.lblTelefono.Size = new System.Drawing.Size(54, 13);
+            this.lblTelefono.Size = new System.Drawing.Size(68, 16);
             this.lblTelefono.TabIndex = 1;
             this.lblTelefono.Text = "Telefonos";
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(30, 38);
+            this.lblNombre.Location = new System.Drawing.Point(40, 47);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
             this.lblNombre.TabIndex = 1;
             this.lblNombre.Text = "Nombre";
             // 
             // btnAceptar
             // 
-            this.btnAceptar.Location = new System.Drawing.Point(504, 359);
+            this.btnAceptar.Enabled = false;
+            this.btnAceptar.Location = new System.Drawing.Point(672, 442);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(87, 26);
+            this.btnAceptar.Size = new System.Drawing.Size(116, 32);
             this.btnAceptar.TabIndex = 2;
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(620, 397);
+            this.ClientSize = new System.Drawing.Size(827, 489);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.mrcTitular);
             this.Controls.Add(this.mrcPagos);
             this.Controls.Add(this.mrcAdicionales);
             this.Controls.Add(this.mrcTipo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reserva de Cabanias";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.mrcTipo.ResumeLayout(false);
             this.mrcTipo.PerformLayout();
             this.mrcAdicionales.ResumeLayout(false);
--- frmMain.cs
+++ frmMain.cs
@@ -11,10 +11,50 @@ using System.Windows.Forms;
 namespace pryArmaniniSp2Ejer1
 {
     public partial class frmMain : Form
+
     {
+
+        // constantes para los cálculos
+ private const float TIPOA = 20;
+ public const float TIPOB = 34;
+ const float COCINA = 1;
+ const float HELADERA = 1.5f;
+ const float TELEVISOR = 2;
+ const float PORPERSONA = 1;
+
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            
+            cmbTipos.Items.Clear();
+            cmbTipos.Items.Add("Tipo A");
+            cmbTipos.Items.Add("Tipo B");
+            
+            cmbTipos.SelectedIndex = 0;
+            
+            txtDias.Text = "";
+            
+            chkCocina.Checked = false;
+            chkHeladera.Checked = false;
+            chkTele.Checked = false;
+            
+            optEfectivo.Checked = true;
+            txtNombre.Text = "";
+            txtTelefono.Text = "";
+            
+            cmbTarjetas.Items.Clear();
+            cmbTarjetas.Items.Add("Card Red");
+            cmbTarjetas.Items.Add("Card Green");
+            cmbTarjetas.Items.Add("Card Blue");
+            
+            btnAceptar.Enabled = false;
+
+
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -24,30 +64,55 @@ namespace pryArmaniniSp2Ejer1
 
         private void cmbTipos_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cmbTipos.SelectedIndex != -1) //es lo mismo que (.Text != "") este hace que si no hay nada no se habilite -1 es que no esta seleccionado
+            //if (cmbTipos.SelectedIndex != -1) //es lo mismo que (.Text != "") este hace que si no hay nada no se habilite -1 es que no esta seleccionado
+            //{
+            //    cmbPersonas.Enabled = true;
+            //}
+            //else
+            //{
+            //    cmbPersonas.Enabled = false;
+            //}
+
+            int I = 0;
+            
+            cmbPersonas.Items.Clear();
+            
+            if (cmbTipos.SelectedIndex == 0) 
             {
-                cmbPersonas.Enabled = true;
+                for (I = 1; I <= 4; I++)
+                {
+                    cmbPersonas.Items.Add(I);
+                }
             }
             else
-            {
-                cmbPersonas.Enabled = false;
+            { 
+                for (I = 1; I <= 8; I++)
+                {
+                    cmbPersonas.Items.Add(I);
+                }
             }
+
+            cmbPersonas.SelectedIndex = 0;
+            
         }
 
         private void cmbPersonas_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cmbPersonas.Text != "")
-            {
-                txtDias.Enabled = true;
-            }
-            else
-            {
-                txtDias.Enabled = false;
-            }
+
+            //if (cmbPersonas.Text != "")
+            //{
+            //    txtDias.Enabled = true;
+            //}
+            //else
+            //{
+            //    txtDias.Enabled = false;
+            //}
+
         }
 
         private void txtDias_TextChanged(object sender, EventArgs e)
         {
+
             if (txtDias.Text != "")
             {
                 mrcAdicionales.Enabled = true;
@@ -58,6 +123,19 @@ namespace pryArmaniniSp2Ejer1
                 mrcAdicionales.Enabled = false;
                 mrcPagos.Enabled = false;
             }
+
+
+            //if (txtDias.Text != "" && txtDias.Text != "0" &&
+            //txtNombre.Text != "" && txtTelefono.Text != "")
+            //{
+            //    btnAceptar.Enabled = true;
+            //}
+            //else
+            //{
+            //    btnAceptar.Enabled = false;
+            //}
+
+
         }
 
         private void optEfectivo_CheckedChanged(object sender, EventArgs e)
@@ -76,13 +154,14 @@ namespace pryArmaniniSp2Ejer1
             }
             else
             {
-                 mrcTitular.Enabled= false;
+                mrcTitular.Enabled = false;
             }
+
         }
 
         private void optTarjeta_CheckedChanged(object sender, EventArgs e)
         {
-            if(optTarjeta.Checked == true)
+            if (optTarjeta.Checked == true)
             {
                 cmbTarjetas.Enabled = true;
             }
@@ -90,6 +169,7 @@ namespace pryArmaniniSp2Ejer1
             {
                 cmbTarjetas.Enabled = false;
             }
+
         }
 
         private void cmbTarjetas_SelectedIndexChanged(object sender, EventArgs e)
@@ -102,6 +182,106 @@ namespace pryArmaniniSp2Ejer1
             else
             {
                 mrcTitular.Enabled = false;
+            }
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "")
+            {
+                txtTelefono.Enabled = true;
+            }
+            else
+            {
+                txtTelefono.Enabled = false;
+            }
+
+        }
+
+        private void txtTelefono_TextChanged(object sender, EventArgs e)
+        {
+            if (txtTelefono.Text != "")
+            {
+                btnAceptar.Enabled = true;
+            }
+            else
+            {
+                btnAceptar.Enabled = false;
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            float PrecioBase;
+            float Opcionales;
+            float Recargo;
+            int Dias;
+            float Total;
+
+            Dias = int.Parse(txtDias.Text);
+
+            if (cmbTipos.SelectedIndex == 0)
+            {
+                PrecioBase = TIPOA;
+            }
+            else
+            {
+                PrecioBase = TIPOB;
+            }
+            
+            PrecioBase = PrecioBase + (PORPERSONA * int.Parse(cmbPersonas.Text));
+           
+            Opcionales = 0;
+            if (chkCocina.Checked == true)
+            {
+                Opcionales = Opcionales + COCINA;
+            }
+            if (chkHeladera.Checked == true)
+            {
+                Opcionales = Opcionales + HELADERA;
+            }
+            if (chkTele.Checked == true)
+            {
+                Opcionales = Opcionales + TELEVISOR;
+            }
+            
+            Total = (PrecioBase + Opcionales) * Dias;
+    
+            if (optTarjeta.Checked == true)
+            {
+                if (cmbTarjetas.SelectedIndex == 0)
+                {
+                    Recargo = Total * 10 / 100;
+                }
+                else
+                {
+                    Recargo = Total * 20 / 100;
+                }
+                Total = Total + Recargo;
+            }
+            
+            MessageBox.Show("Total = " + Total.ToString(), "Importe de la reserva",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void txtDias_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtTelefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
             }
         }
     }
